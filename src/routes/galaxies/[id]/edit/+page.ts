f48280d634a7a16502636318cf917a7cf776178c
Kresna Satya <kresnasatya72@gmail.com>
2025-07-09 09:23:36 +0800

    refactor: replace [slug] with [id] for consistency

--- src/routes/galaxies/[id]/edit/+page.ts
+++ src/routes/galaxies/[id]/edit/+page.ts
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params, fetch }) => {
-    const id = params.slug;
+    const id = params.id;
 
     if (isNaN(Number(id))) {
         throw error(400, 'Invalid galaxy ID');
